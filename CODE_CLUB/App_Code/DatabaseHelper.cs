1439e8f029c74d9ed86ffc9186d3635abcc2fe3d
Shohana Akter Rabina <shohana592059@gmail.com>
2026-05-10 20:28:10 +0600

    Project creation with necessary folder

---
+++ CODE_CLUB/App_Code/DatabaseHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
+
+public static class DatabaseHelper
+{
+    private static string ConnStr => ConfigurationManager.ConnectionStrings["CODE_CLUBDB"].ConnectionString;
+
+    private static SqlConnection GetConnection()
+    {
+        var conn = new SqlConnection(ConnStr);
+        conn.Open();
+        return conn;
+    }
+
+
+}
---
+++ CODE_CLUB/App_Code/SessionManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CODE_CLUB.App_Code
+{
+    public class SessionManager
+    {
+    }
+}
